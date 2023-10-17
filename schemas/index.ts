50eba229b01dc056131c1cd66fbc13cff13a4d4a
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2023-10-17 17:18:51 +0200

    Improved schemas with posts, sources and categories

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import source from './source'
+
+export const schemaTypes = [post, source, category, blockContent]
---
+++ schemas/post.ts
@@ -0,0 +1,65 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'source',
+      title: 'Source',
+      type: 'reference',
+      to: {type: 'source'},
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      source: 'source.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {source} = selection
+      return {...selection, subtitle: source && `by ${source}`}
+    },
+  },
+})
