7a48596295f974c16ce556e8a5b370106660c01b
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2023-10-24 06:13:48 +0200

    Relational schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 // import {defineConfig} from 'sanity'
-import {visionTool} from '@sanity/vision'
+import { visionTool } from '@sanity/vision'
 import {
   apiVersion,
   dataset,
@@ -14,14 +14,23 @@ import { deskTool } from 'sanity/desk'
 import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
 import { previewUrl } from 'sanity-plugin-iframe-pane/preview-url'
 
-import blockContentType from './schemas/blockContent'
-import categoryType from './schemas/category'
+import careerType from './schemas/career'
 import sectionType from './schemas/section'
+import categoryType from './schemas/category'
 import postType from './schemas/post'
+import blockContentType from './schemas/blockContent'
 import authorType from './schemas/author'
 import settingsType from './schemas/settings'
 
-const schemaTypes = [authorType, categoryType, sectionType, blockContentType, postType, settingsType]
+const schemaTypes = [
+  careerType,
+  sectionType,
+  categoryType,
+  postType,
+  blockContentType,
+  authorType,
+  settingsType
+]
 
 export default defineConfig({
   name: 'default',
@@ -34,21 +43,15 @@ export default defineConfig({
   plugins: [
     deskTool({
       structure: settingsStructure(settingsType),
-      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
       defaultDocumentNode: previewDocumentNode(),
     }),
-    // Configures the global "new document" button, and document actions, to suit the Settings document singleton
     settingsPlugin({ type: settingsType.name }),
-    // Add the "Open preview" action
     previewUrl({
       base: DRAFT_MODE_ROUTE,
       urlSecretId: previewSecretId,
       matchTypes: [postType.name, settingsType.name],
     }),
-    // Add an image asset source for Unsplash
     unsplashImageAsset(),
-    // Vision lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
   ],
 })
